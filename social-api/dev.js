bd5412e1443b117c1783b4fd131f649c77f05cc8
Ei Maung <eimg@fairwayweb.com>
2025-10-04 09:39:33 +0630

    social api login

--- social-api/dev.js
+++ social-api/dev.js
@@ -1,10 +1,28 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-async function main() {
-    const password = "apple";
-	const hash = await bcrypt.hash(password, 10);
+const user = { name: 'Alice', age: 22 };
+const secret = "some secret code";
 
-    console.log(hash);
+function createToken() {
+    const token = jwt.sign(user, secret);
+    console.log(token);
 }
 
-main();
+function decodeToken() {
+    const token =
+		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWxpY2UiLCJhZ2UiOjIyLCJpYXQiOjE3NTk1NDYzMTl9.71oCKqa0_IYRfYwPiZzVYf3Cb8nJ8AGzl3sP5j_-jdg";
+    console.log(jwt.decode(token, secret));
+}
+
+createToken();
+decodeToken();
+
+// async function main() {
+//     const password = "apple";
+// 	const hash = await bcrypt.hash(password, 10);
+
+//     console.log(hash);
+// }
+
+// main();
